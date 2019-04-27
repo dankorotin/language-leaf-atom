65504bd2601211cdf16c9e650b868cf3139f8f1b
Daniil Korotin <korotin.daniil@gmail.com>
2019-04-27 16:18:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# language-leaf-atom package
+
+Leaf templating language code completion and highlighting package for Atom
---
+++ spec/language-leaf-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageLeafAtomView from '../lib/language-leaf-atom-view';
+
+describe('LanguageLeafAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
