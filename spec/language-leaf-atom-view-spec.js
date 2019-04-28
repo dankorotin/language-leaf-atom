09e164e40e831ab36bceca1ea368f2e969976e21
Daniil Korotin <korotin.daniil@gmail.com>
2019-04-28 20:25:59 +0300

    Add HTML injection

--- spec/language-leaf-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageLeafAtomView from '../lib/language-leaf-atom-view';
-
-describe('LanguageLeafAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
